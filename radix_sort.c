25ad1b7572ea4bba579e7913a4438df8e97d27eb
Ivan <wwwvania9060@gmail.com>
2025-06-30 17:23:45 +0300

    changes for limits

--- radix_sort.c
+++ radix_sort.c
@@ -34,7 +34,7 @@ void ensureCapacity(NumberArray* arr, int minCapacity);
 
 int main() {
     setlocale(LC_ALL, "Rus");
-    NumberArray arr = {NULL, 0, 0};
+    NumberArray arr = { NULL, 0, 0 };
     char choice;
 
     // Выделение начальной памяти для массива
@@ -52,22 +52,22 @@ int main() {
         scanf(" %c", &choice);
 
         switch (choice) {
-            case '1':
-                getNums(&arr);
-                sortIfNeeded(&arr);
-                break;
-            case '2':
-                autoGen(&arr);
-                sortIfNeeded(&arr);
-                break;
-            case '3':
-                free(arr.data);
-                printf("Программа завершена.\n");
-                return 0;
-            default:
-                printf("Неверный ввод. Пожалуйста, выберите 1, 2 или 3.\n");
-                printf("\nНажмите Enter чтобы продолжить...");
-                getchar(); getchar();
+        case '1':
+            getNums(&arr);
+            sortIfNeeded(&arr);
+            break;
+        case '2':
+            autoGen(&arr);
+            sortIfNeeded(&arr);
+            break;
+        case '3':
+            free(arr.data);
+            printf("Программа завершена.\n");
+            return 0;
+        default:
+            printf("Неверный ввод. Пожалуйста, выберите 1, 2 или 3.\n");
+            printf("\nНажмите Enter чтобы продолжить...");
+            getchar(); getchar();
         }
     }
 }
@@ -104,11 +104,11 @@ void sortIfNeeded(NumberArray* arr) {
         printf("\nСгенерированные числа сохранены в файл \"%s\"\n", unsortedFilename);
         printf("\nСгенерированные числа:\n");
         printNums(arr);
-        
+
         printf("\nХотите отсортировать числа? (y/n): ");
         char sortChoice;
         scanf(" %c", &sortChoice);
-        
+
         if (sortChoice == 'y' || sortChoice == 'Y') {
             printf("Введите имя файла для сохранения отсортированных чисел: ");
             scanf("%99s", sortedFilename);
@@ -117,11 +117,11 @@ void sortIfNeeded(NumberArray* arr) {
             radixSort(arr->data, arr->count);
             printf("\nОтсортированные числа:\n");
             printNums(arr);
-            
+
             saveNumsToFile(arr, sortedFilename);
             printf("\nОтсортированные числа сохранены в файл \"%s\"\n", sortedFilename);
         }
-        
+
         printf("\nНажмите Enter чтобы продолжить...");
         getchar(); getchar();
     }
@@ -140,62 +140,62 @@ void showMainMenu() {
 void getNums(NumberArray* arr) {
     clearScreen();
     printf("Введите числа от -10000 до 10000 (для завершения введите 'end'):\n");
-    
+
     arr->count = 0;
     char input[MAX_INPUT_LENGTH];
-    
+
     while (1) {
         printf("Число #%d: ", arr->count + 1);
         scanf("%s", input);
-        
+
         if (strcmp(input, "end") == 0) {
             break;
         }
-        
+
         if (!isInteger(input)) {
             printf("Ошибка! Введите целое число от -10000 до 10000.\n");
             continue;
         }
-        
+
         int num = atoi(input);
         if (num < -10000 || num > 10000) {
             printf("Число должно быть в диапазоне от -10000 до 10000.\n");
             continue;
         }
-        
+
         // Проверка и при необходимости увеличение вместимости
         ensureCapacity(arr, arr->count + 1);
         arr->data[arr->count++] = num;
     }
-    
+
     printf("\nВведено %d чисел.\n", arr->count);
 }
 
 void autoGen(NumberArray* arr) {
     clearScreen();
     int min, max, n;
-    
+
     printf("Введите минимальное значение: ");
     scanf("%d", &min);
     printf("Введите максимальное значение: ");
     scanf("%d", &max);
     printf("Введите количество чисел: ");
     scanf("%d", &n);
-    
+
     if (min > max) {
         int temp = min;
         min = max;
         max = temp;
     }
-    
+
     ensureCapacity(arr, n);
     arr->count = n;
-    
+
     srand(time(NULL));
     for (int i = 0; i < n; i++) {
         arr->data[i] = rand() % (max - min + 1) + min;
     }
-    
+
     printf("\nСгенерировано %d чисел в диапазоне от %d до %d.\n", n, min, max);
 }
 
@@ -205,11 +205,11 @@ void saveNumsToFile(NumberArray* arr, const char* filename) {
         printf("Ошибка сохранения в файл '%s'.\n", filename);
         return;
     }
-    
+
     for (int i = 0; i < arr->count; i++) {
         fprintf(file, "%d\n", arr->data[i]);
     }
-    
+
     fclose(file);
 }
 
@@ -218,14 +218,14 @@ void loadNumsFromFile(NumberArray* arr, const char* filename) {
     if (!file) {
         return;
     }
-    
+
     arr->count = 0;
     int num;
     while (fscanf(file, "%d", &num) == 1) {
         ensureCapacity(arr, arr->count + 1);
         arr->data[arr->count++] = num;
     }
-    
+
     fclose(file);
 }
 
@@ -237,49 +237,93 @@ void printNums(NumberArray* arr) {
     printf("\n");
 }
 
-int getMax(int* arr, int n) {
-    int mx = arr[0];
-    for (int i = 1; i < n; i++)
-        if (arr[i] > mx)
-            mx = arr[i];
-    return mx;
+int getMaxAbs(int* arr, int n) {
+    int max_abs = abs(arr[0]);
+    for (int i = 1; i < n; i++) {
+        int current_abs = abs(arr[i]);
+        if (current_abs > max_abs) {
+            max_abs = current_abs;
+        }
+    }
+    return max_abs;
 }
 
-// Вспомогательная функция сортировки подсчетом для поразрядной
 void countSort(int* arr, int n, int exp) {
     int* output = (int*)malloc(n * sizeof(int));
-    int i, count[10] = {0};
-    
-    // Смещение для отрицательных
-    int bias = 0;
-    int min = 0;
-    for (i = 0; i < n; i++) {
-        if (arr[i] < min) min = arr[i];
+    int count[10] = { 0 };  // Для цифр 0-9
+
+    // Подсчет количества чисел для каждой цифры
+    for (int i = 0; i < n; i++) {
+        count[(arr[i] / exp) % 10]++;
     }
-    if (min < 0) bias = -min;
-    
-    for (i = 0; i < n; i++)
-        count[((arr[i] + bias) / exp) % 10]++;
-    
-    for (i = 1; i < 10; i++)
+
+    // Преобразуем count в позиции в output[]
+    for (int i = 1; i < 10; i++) {
         count[i] += count[i - 1];
-    
-    for (i = n - 1; i >= 0; i--) {
-        output[count[((arr[i] + bias) / exp) % 10] - 1] = arr[i];
-        count[((arr[i] + bias) / exp) % 10]--;
     }
-    
-    for (i = 0; i < n; i++)
+
+    // Строим выходной массив (идем с конца для стабильности)
+    for (int i = n - 1; i >= 0; i--) {
+        output[count[(arr[i] / exp) % 10] - 1] = arr[i];
+        count[(arr[i] / exp) % 10]--;
+    }
+
+    // Копируем output обратно в arr[]
+    for (int i = 0; i < n; i++) {
         arr[i] = output[i];
-    
+    }
+
     free(output);
 }
 
 void radixSort(int* arr, int n) {
-    int m = getMax(arr, n);
-    
-    for (int exp = 1; m / exp > 0; exp *= 10)
-        countSort(arr, n, exp);
+    if (n <= 1) return;  // Не нужно сортировать пустой массив или из 1 элемента
+
+    // 1. Разделяем отрицательные и положительные числа
+    int* neg = (int*)malloc(n * sizeof(int));
+    int* pos = (int*)malloc(n * sizeof(int));
+    int neg_size = 0, pos_size = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] < 0)
+            neg[neg_size++] = -arr[i];  // Инвертируем отрицательные
+        else
+            pos[pos_size++] = arr[i];
+    }
+
+    // 2. Сортируем отрицательные числа (как положительные, затем разворачиваем)
+    if (neg_size > 0) {
+        // Используем getMaxAbs() и countSort() для неотрицательных
+        int max_abs = getMaxAbs(neg, neg_size);
+        for (int exp = 1; max_abs / exp > 0; exp *= 10) {
+            countSort(neg, neg_size, exp);
+        }
+        // Разворачиваем порядок (т.к. изначально числа были отрицательными)
+        for (int i = 0; i < neg_size / 2; i++) {
+            int temp = neg[i];
+            neg[i] = neg[neg_size - 1 - i];
+            neg[neg_size - 1 - i] = temp;
+        }
+        // Возвращаем знак "-"
+        for (int i = 0; i < neg_size; i++) {
+            arr[i] = -neg[i];
+        }
+    }
+
+    // 3. Сортируем положительные числа
+    if (pos_size > 0) {
+        int max_abs = getMaxAbs(pos, pos_size);
+        for (int exp = 1; max_abs / exp > 0; exp *= 10) {
+            countSort(pos, pos_size, exp);
+        }
+        // Копируем их в исходный массив после отрицательных
+        for (int i = 0; i < pos_size; i++) {
+            arr[neg_size + i] = pos[i];
+        }
+    }
+
+    free(neg);
+    free(pos);
 }
 
 int isInteger(const char* input) {
